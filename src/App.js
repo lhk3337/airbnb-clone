44c370f656aced9d25535a7d6820f941b2b1a32a
lhk3337 <lhk3337@naver.com>
2020-09-12 18:35:34 +0900

    react router add

--- src/App.js
+++ src/App.js
@@ -3,12 +3,23 @@ import "./style/App.css";
 import Header from "./component/Header";
 import Home from "./component/Home";
 import Footer from "./component/Footer";
+import SearchPage from "./component/SearchPage";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
-      <Footer />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
